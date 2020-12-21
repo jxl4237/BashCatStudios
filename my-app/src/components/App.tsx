c8130e9e35140d6ab544a535f251628b918c3ce5
Jacob Lambert <jtlambert99@gmail.com>
2020-12-21 11:22:58 -0500

    added new components for the header tab content

---
+++ my-app/src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.scss";
+import Header from "./components/Header";
+
+export default function App(): JSX.Element {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
---
+++ my-app/src/components/components/components/About.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function About(): JSX.Element {
+  return <>About Me<br />..............</>;
+}
